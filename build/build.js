557a65a1efe374692db5d9bb077bcffff4477193
Roland Bernard <rolli.bernard@gmail.com>
2020-11-26 15:26:17 +0000

    Added the build script

---
+++ build/build.js
@@ -0,0 +1,16 @@
+
+import { Builder } from './builder.js';
+        
+import { notFoundView } from '../src/views/not-found-view.js';
+
+const debug = process.argv.includes('-d');
+
+const builder = new Builder('dist/', { debug: debug });
+
+builder.copyDirContent('assets/');
+
+for(const lang of ['', 'en', 'de', 'it']) {
+
+}
+
+builder.addHtmlFile(`404.html`, notFoundView());
